dae257e80370c4e5f01c2f16acc1d3552195e7fc
Abdul Hannan <abdulhannanhere@gmail.com>
2025-10-17 19:43:31 +0500

    update

--- client/src/pages/user/Auth.jsx
+++ client/src/pages/user/Auth.jsx
@@ -3,7 +3,7 @@ import LoginForm from "@/components/user/LoginForm";
 const Auth = () => {
   return (
     <div className="grid min-h-svh lg:grid-cols-2 p-5">
-      <div className="flex flex-col gap-6 p-6 md:p-2 items">
+      <div className="flex flex-col gap-6 p-6 md:p-2">
         <div className="flex justify-center gap-2 md:justify-start">
           <a href="#" className="flex items-center gap-2 font-medium">
             <div className="text-primary-foreground flex size-10 items-center justify-center rounded-md">
